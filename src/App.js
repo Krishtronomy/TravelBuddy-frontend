71686474fd0e42e2d69bb471966c54cc58e21fdc
James Xu <jamesxu_1996@hotmail.com>
2022-07-17 01:16:33 +1000

    File Restructure, Navbar & Wrapper implemented

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from "react";
-import Navigation from "./components/Navigation";
+// import Navigation from "./components/Navbar/Navigation";
+import { Navbar } from "./components";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import Travels from "./pages/Travels";
-import Profile from "./pages/Profile";
-import LoginForm from "./pages/LoginForm";
-import AppCss from "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Navigation />
-          <Routes>
-            <Route exact path="pages/home" element={<Home />} />
-            <Route path="pages/explore" element={<Explore />} />
-            <Route path="pages/travels" element={<Travels />} />
-            <Route path="pages/profile" element={<Profile />} />
-            <Route path="pages/login" element={<LoginForm />} />
-          </Routes>
-        </Router>
-      </header>
-    </div>
-  );
-}
+import { Home, Explore, Travels, Profile, Login } from "./pages";
+
+// import Home from "./pages/Home/Home";
+// import Explore from "./pages/Explore/Explore";
+// import Travels from "./pages/Travels/Travels";
+// import Profile from "./pages/Profile/Profile";
+// import LoginForm from "./pages/Login/LoginForm";
+
+import "./App.scss";
+// import AppCss from "./styles/App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Router>
+//           <Navbar />
+//           <Routes>
+//             <Route exact path="pages/home/home" element={<Home />} />
+//             <Route path="pages/explore/explore" element={<Explore />} />
+//             <Route path="pages/travels/travels" element={<Travels />} />
+//             <Route path="pages/profile/profile" element={<Profile />} />
+//             <Route path="pages/login/login" element={<LoginForm />} />
+//           </Routes>
+//         </Router>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => (
+	<div className="app">
+		<Navbar />
+		<Home />
+		<Explore />
+		<Travels />
+		<Profile />
+	</div>
+);
 
 export default App;
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { motion } from "framer-motion";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./Profile.scss";
+
+const Profile = () => {
+    return (
+        <>
+            <h1>Profile page</h1>
+        </>
+    )
+}
+
+export default AppWrap(Profile, "profile");
