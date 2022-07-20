734ad2f02801e247ea0fe5fb5960302b4e3659c4
Krish <krishal@myxplor.com>
2022-07-20 13:30:23 +1000

    Added event handlers for NewPost form

--- src/pages/Travels/NewPost.jsx
+++ src/pages/Travels/NewPost.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Travels.scss";
 
 const NewPost = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const post = {title, description}
+    
+  };
   return (
     <>
       <div className="create">
-        <h1>Create Post</h1>
-        <form>
+        <h2>Create Post</h2>
+        <form onSubmit={handleSubmit}>
           <label>Title:</label>
-          <input type="text" required />
-          <label>Description</label>
-          <textarea required></textarea>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={handleTitleChange}
+          />
+          <label>Description:</label>
+          <textarea required onChange={handleDescriptionChange}></textarea>
+          <label>Add Image:</label>
+          <input className="file" type="file" />
           <button>Submit</button>
         </form>
       </div>
+      <h1>{title}</h1>
+      {description}
     </>
   );
 };
