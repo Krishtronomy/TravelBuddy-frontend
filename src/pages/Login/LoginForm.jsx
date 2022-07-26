0c2acc8f892b25dd0d6293a729644e3f788f1cad
Krish <krishal@myxplor.com>
2022-07-26 19:50:54 +1000

    Added logic rendering for logged in/out user and added sign out handler

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -7,44 +7,69 @@ import postAPI from "../../config/api";
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loggedInUser, setLoggedInUser] = useState("")
+  const [loggedInUser, setLoggedInUser] = useState(
+    sessionStorage.getItem("user") || null
+  );
+  const [loginError, setLoginError] = useState(null);
 
+  //   Set email state to user typed entry on input form
   const emailChangeHandler = (event) => {
     setEmail(event.target.value);
   };
 
+  //   Set password state to user typed entry on input form
   const passwordChangeHandler = (event) => {
     setPassword(event.target.value);
   };
 
+//   Sets sign in details and verifies with the database if details are correct 
   const submitFormHandler = (event) => {
     event.preventDefault();
     const login = { email, password };
-    postAPI.post("/sign_in", login)
-    .then(response => {
-        console.log(response.data)
-        // sessionStorage.setItem("token", jwt)
-        setLoggedInUser(response.data.username)
-    })
+    postAPI
+      .post("/sign_in", login)
+      .then((response) => {
+        sessionStorage.setItem("token", response.data.jwt);
+        sessionStorage.setItem("user", response.data.username);
+        setLoggedInUser(response.data.username);
+        setEmail("");
+        setPassword("");
+        setLoginError(null);
+      })
+      .catch((error) => {
+        setLoginError(error.response.data.error);
+      });
+  };
 
+//   Handles signing out
+  const signOutHandler = () => {
+    sessionStorage.clear();
+    setLoggedInUser(null);
   };
   return (
     <>
       <h1>Login Page</h1>
-      <h2>{loggedInUser && loggedInUser}</h2>
+      {loggedInUser && <h3>Logged in: {loggedInUser}</h3>}
+      {loggedInUser && <button onClick={signOutHandler}>Sign Out</button>}
       <div>
         <form onSubmit={submitFormHandler}>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={emailChangeHandler} />
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={passwordChangeHandler}
-          />
-          <div>
-            <button>Login</button>
-          </div>
+          {!loggedInUser && (
+            <div>
+              <label>Email:</label>
+              <input type="email" value={email} onChange={emailChangeHandler} />
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={passwordChangeHandler}
+              />
+
+              <div>
+                <button>Login</button>
+                {loginError && <p style={{ color: "red" }}>{loginError}</p>}
+              </div>
+            </div>
+          )}
         </form>
       </div>
     </>
